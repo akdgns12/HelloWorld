8561c2c355aca1b8185ef53dd9efded8922e4bc0
byeonyujeong <bbnt25@naver.com>
2023-04-26 17:40:22 +0900

    [Feat] 알림창 UI && 게시글 share UI 진행중

--- FE/src/main.js
+++ FE/src/main.js
@@ -2,10 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import {router} from './router/index.js'
 import VueSplide from '@splidejs/vue-splide';
+import timeago from 'vue-timeago3';
+import { ko } from 'date-fns/locale';
 
 //createApp(App).mount('#app')
 
 const app = createApp(App);
+const timeagoOptions = {
+    converterOptions: {
+        includeSeconds : true,
+    },
+    locale: ko,
+}
+
 app.use(router)
 app.use(VueSplide)
+app.use(timeago, timeagoOptions)
 app.mount("#app")
